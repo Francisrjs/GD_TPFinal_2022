95df781c48aff5d50de2369b297bef35f6eb81d7
Francisrjs <47805559+Francisrjs@users.noreply.github.com>
2022-10-21 16:00:08 -0300

    Agrego funciones, simplifico codigo y explicación

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
 const {Pool}=require('pg'); //PGadmin
 const moment = require('moment');  //Fecha y Hora al momento
 const fs= require('fs'); //para hacer el guardado de los archivos
-var querys
 const config= {
     host: 'localhost',
     user: 'postgres',
-    password: '123', //si tiene contraseña 
-    database: 'final' //acá va el nombre de la base de datos
+    password: '123', //si tiene contraseña el postgres
+    database: 'ejercicio_integrador' //acá va el nombre de la base de datos
 }
 //Procedimiento para obtener el tiempo
-
+var prueba ="SELECT fvapnam, ftscprd  FROM sc_e005.p_appapp_dat;"
+//----
+//Caso importante  en el 3er 4to parametro debemos indicar el esquema para eso ponemos 
+// esquema.Tabla
+//----
+var ejemploCopyTable="SELECT sc_e002.copy_table( 'sc_e002', 'sc_e002', 'sc_e002.p_icrdd', 'sc_e002.p_icrbd' );";
 const date = new Date();
 const pool=new Pool(config);
-var query ='SELECT fvapnam, ftscprd  FROM sc_e005.p_appapp_dat'
+var query=prueba;
+
 const getTabla = async()=>{
     try{
  console.log(moment().format('MMMM DD YYYY, h:mm:ss a'));
-    const res= await (await pool.query(query));
+    pool.query("SET search_path TO 'sc_e002';") 
+    const res= (await pool.query(query));
+    //Proceso de guardado
+    fs.appendFileSync('LogsPostgres.txt','\n');
+    fs.appendFileSync('LogsPostgres.txt',moment().format('MMMM DD YYYY, h:mm:ss a'));
+    fs.appendFileSync('LogsPostgres.txt','\n '&&query);
     query=res.rows;
     console.log(query);
     } catch (err){
+        //Seccion de erroresds
        console.log(err);
+       fs.appendFileSync('LogsPostgres.txt','\n');
+       fs.appendFileSync('LogsPostgres.txt',"Error");
+       fs.appendFileSync('LogsPostgres.txt','\n');
     }
    
 }
 //client es para una conexión y pool es para varías conexiones, Queary es consultar cosas
-getTabla()
-            .then(console.log)
-            .catch(console.error);
-try{
-    fs.appendFileSync('LogsPostgres.json','\n');
-    fs.appendFileSync('LogsPostgres.json',moment().format('MMMM DD YYYY, h:mm:ss a'));
-    fs.appendFileSync('LogsPostgres.json','\n '&&query); //No me deja meter el resultado de la
-     
-    //fs.writeFileSync('./quotes.json',JSON.stringify(quotes),'utf8')
-}   catch(err){
-    console.log(err)
-}
+getTabla();
 
 //Conectarse a postgres vía windows psql -U postgres , vía linux sudo -u postgres psql. PARA SALIR \q
 
